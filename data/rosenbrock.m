d5d41ff73d5e738e5bbf9aa0b6e654b28fb00009
Oliver Woodford <o.j.woodford.98@cantab.net>
2015-05-10 11:37:15 +0100

    Make cost function a class

--- data/rosenbrock.m
+++ data/rosenbrock.m
@@ -9,14 +9,51 @@
 % r - 2xN set of residuals at the N input coordinates.
 % J - 2x2xN set of residual Jacobians at the N input coordinates.
 
-function [r, J] = rosenbrock(x)
-a = 1;
-sqrt_b = 10;
-y = x(2,:);
-x = x(1,:);
-r = [x - a; sqrt_b * (y - x .* x)];
-if nargout > 1
-    J = repmat([1 0; 0 sqrt_b], numel(x));
-    J(2,1,:) = (-2 * sqrt_b) * shiftdim(x, -1);
-end
+classdef rosenbrock
+    properties
+        a;
+        sqrt_b;
+    end
+    
+    methods
+        function this = rosenbrock(a, b)
+            if nargin < 2
+                b = 100;
+                if nargin < 1
+                    a = 1;
+                end
+            end
+            this.a = a;
+            this.sqrt_b = sqrt(b);
+        end
+        
+        function [r, J] = residuals(this, x)
+            y = x(2,:);
+            x = x(1,:);
+            r = [x - this.a; this.sqrt_b * (y - x .* x)];
+            if nargout > 1
+                J = repmat([1 0; 0 this.sqrt_b], numel(x));
+                J(2,1,:) = (-2 * this.sqrt_b) * shiftdim(x, -1);
+            end
+        end
+        
+        function c = cost(this, x)
+            c = residuals(this, x);
+            c = sum(c .* c)';
+        end
+        
+        function h = render(this, num, range)
+            if nargin < 3
+                range = [-3 3 -3 3];
+                if nargin < 2
+                    num = 100;
+                end
+            end
+            [y, x] = ndgrid(linspace(range(3), range(4), num(min(2, end))), linspace(range(1), range(2), num(1)));
+            x = log1p(cost(this, [x(:)'; y(:)']));
+            x = reshape(x / max(x), size(y));
+            h = image(range(1:2), range(3:4), x, 'CDataMapping', 'scaled');
+            colormap(parula(24));
+        end
+    end
 end
--- demos/optimization_demo.m
+++ demos/optimization_demo.m
@@ -1,10 +1,13 @@
 %OPTIMIZATION_DEMO Demonstrate optimization of the Rosenbrock function
-function optimization_demo(x)
-if nargin < 1
-    x = [-3; 3];
+function optimization_demo(x, cost_func)
+if nargin < 2
+    cost_func = rosenbrock();
+    if nargin < 1
+        x = [-3; 3];
+    end
 end
 % Optimize
-h = gauss_newton(@rosenbrock, x);
+h = gauss_newton(@(x) residuals(cost_func, x), x);
 [x(:,end+1), s] = iterate(h);
 for a = 1:100
     [x(:,end+1), s_] = iterate(h);
@@ -18,12 +21,8 @@ x = x';
 % Compute the cost function over the range specified
 n = 1000;
 range = [min(x)-1; max(x)+2];
-[Y, X] = ndgrid(linspace(range(3), range(4), n), linspace(range(1), range(2), n));
-C = rosenbrock([X(:)'; Y(:)']);
-C = reshape(sum(C .* C, 1), n, n);
 clf;
-image(range(1:2), range(3:4), log1p(C) ./ log1p(max(C(:))), 'CDataMapping', 'scaled');
-colormap(parula(24));
+render(cost_func, n, range);
 hold on
 plot(x(:,1), x(:,2), 'k-x');
 end
